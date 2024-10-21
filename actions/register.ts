3a4135fd62a1677912caff9e9f48b355777dd9d7
jexhsu <jexhsu@gmail.com>
2024-10-22 00:34:56 +0800

    create user

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,9 @@
 "use server";
 
+import { getUserByEmail } from "@/data/user";
+import { prisma } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcryptjs";
 import { z } from "zod";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
@@ -8,5 +11,19 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     if (!validatedFields.success) {
         return { error: "Invalid fields!" };
     }
-    return { success: "Email sent!" };
+    const { email, password, name } = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const existingUser = await getUserByEmail(email);
+    if (existingUser) {
+        return { error: "Email already in use!" };
+    }
+    await prisma.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        },
+    });
+    // TODO: Send verification token email
+    return { success: "User created!" };
 };
